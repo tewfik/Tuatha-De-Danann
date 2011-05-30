40d418b30d325582daa102f4bcb55b02a5b2b51b
MikaYuoadas <mikayuoadas@gmail.com>
2011-05-30 18:44:45 +0200

    change tiles and the way area are generated (made it easier to use)

--- shared/area.py
+++ shared/area.py
@@ -3,12 +3,35 @@
 
 import sys
 import pickle
+from random import randint
 import pygame
 from pygame.locals import *
 
 SQUARE_SIZE = 32
 COLUMNS = 32
 ROWS = 24
+
+VOID = 0
+GRASS = 1
+DIRT = 2
+MIXED = 3
+
+T_BRUSHES = 3
+T_TALL_GRASS = 4
+T_GRASS = 0
+T_DIRT = 17
+T_DIRT_R = 5
+T_DIRT_L = 6
+T_DIRT_U = 7
+T_DIRT_D = 8
+T_DIRT_DR = 9
+T_DIRT_DL = 10
+T_DIRT_UL = 11
+T_DIRT_UR = 12
+T_DIRT_GDR = 13
+T_DIRT_GDL = 14
+T_DIRT_GUL = 15
+T_DIRT_GUR = 16
 
 class Area():
     """
@@ -54,14 +77,82 @@ if __name__ == '__main__':
         print 'usage: ./area.py <source>'
         sys.exit()
 
-    f = open(sys.argv[1], 'r')
     area.map = [[0]* COLUMNS for x in xrange(ROWS)]
+    map = [[0]* COLUMNS for x in xrange(ROWS)]
+    f = open(sys.argv[1], 'r')
     for i in xrange(0, ROWS):
         line = f.readline()[:-1]
         row = line.split(' ')
         for j in xrange(0, COLUMNS):
-            area.map[i][j] = int(row[j])
+            map[i][j] = int(row[j])
     f.close()
+
+    for i in xrange(0, ROWS):
+        for j in xrange(0, COLUMNS):
+            if map[i][j] == GRASS:
+                if randint(0, 30):
+                    area.map[i][j] = T_GRASS
+                elif randint(0, 2):
+                    area.map[i][j] = T_BRUSHES
+                else:
+                    area.map[i][j] = T_TALL_GRASS
+            elif map[i][j] == MIXED:
+                    if i <= 0:
+                        up = VOID
+                    else:
+                        up = map[i - 1][j]
+                    if i >= ROWS - 1:
+                        down = VOID
+                    else:
+                        down = map[i + 1][j]
+                    if j <= 0:
+                        left = VOID
+                    else:
+                        left = map[i][j - 1]
+                    if j >= COLUMNS - 1:
+                        right = VOID
+                    else:
+                        right = map[i][j + 1]
+
+                    MIXEDV = (MIXED, VOID)
+                    DIRTM = (DIRT, MIXED)
+                    # Sides of the road
+                    if up in DIRTM and down == GRASS and left in MIXEDV and right in MIXEDV:
+                        area.map[i][j] = T_DIRT_U
+                    elif up in MIXEDV and down in MIXEDV and left == GRASS and right in DIRTM:
+                        area.map[i][j] = T_DIRT_R
+                    elif up == GRASS and down in DIRTM and left in MIXEDV and right in MIXEDV:
+                        area.map[i][j] = T_DIRT_D
+                    elif up in MIXEDV and down in MIXEDV and left in DIRTM and right == GRASS:
+                        area.map[i][j] = T_DIRT_L
+
+                    # Corners of the road
+                    elif up == GRASS and down in MIXEDV and left == GRASS and right in MIXEDV:
+                        area.map[i][j] = T_DIRT_DR
+                    elif up == GRASS and down in MIXEDV and left in MIXEDV and right == GRASS:
+                        area.map[i][j] = T_DIRT_DL
+                    elif up in MIXEDV and down == GRASS and left in MIXEDV and right == GRASS:
+                        area.map[i][j] = T_DIRT_UL
+                    elif up in MIXEDV and down == GRASS and left == GRASS and right in MIXEDV:
+                        area.map[i][j] = T_DIRT_UR
+
+                    # Curves of the road
+                    elif map[i + 1][j + 1] == GRASS:
+                        area.map[i][j] = T_DIRT_GDR
+                    elif map[i + 1][j - 1] == GRASS:
+                        area.map[i][j] = T_DIRT_GDL
+                    elif map[i - 1][j - 1] == GRASS:
+                        area.map[i][j] = T_DIRT_GUL
+                    elif map[i - 1][j + 1] == GRASS:
+                        area.map[i][j] = T_DIRT_GUR
+
+                    # Center of the road
+                    else:
+                        area.map[i][j] = T_DIRT
+            elif map[i][j] == DIRT:
+                area.map[i][j] = T_DIRT
+            else:
+                area.map[i][j] = T_GRASS
 
     mapname = raw_input("map name : ")
     f_map = open(mapname+'.map', 'w')
