961c0b1bb011601ad1a091031cd50bc2aef033c9
MikaYuoadas <mikayuoadas@gmail.com>
2011-05-31 12:41:00 +0200

    Added pyrotechnic sprite.

--- Etain/particle.py
+++ Etain/particle.py
@@ -15,9 +15,9 @@ class Particle:
         self.current_frame = -1
         self.ttl = ttl
         self.dead = False
-        self.pos = (pos[0] - 50 , pos[1] - 15)
         self.frames = []
         if type == 'dmg':
+            self.pos = (pos[0] - 50 , pos[1] - 15)
             msg = '- ' + params[0]
             for i in xrange(self.ttl/4 + 1):
                 surf = pygame.Surface((100, 50), HWSURFACE | SRCALPHA)
@@ -29,6 +29,14 @@ class Particle:
                 text_Rect = text.get_rect()
                 text_Rect.center = (50, 25)
                 self.frames[i].blit(text, text_Rect)
+        else:
+            f = open('sprites/' + type + '.png', 'r')
+            data = f.readline().split('**')
+            f.close()
+            self.pos = (pos[0] - int(data[0])/2 , pos[1] - int(data[1])/2)
+            self.frame_rate = int(data[2])
+            for path in data[3:]:
+                pass #TODO
 
 
     def update(self):
