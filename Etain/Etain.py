370fcddb4f5d64fc1c42e6caefbd81d1a4984ed3
MikaYuoadas <mikayuoadas@gmail.com>
2011-05-31 15:32:26 +0200

    Added pseudo support to chat.

--- Etain/Etain.py
+++ Etain/Etain.py
@@ -11,15 +11,22 @@ from locales import *
 
 if __name__ == '__main__':
     # command line argument management
-    if(len(sys.argv) >= 3):
-        host = sys.argv[1]
-        port = int(sys.argv[2])
-    elif(len(sys.argv) == 2):
+    if len(sys.argv) >= 4:
+        pseudo = sys.argv[1]
+        host = sys.argv[2]
+        port = int(sys.argv[3])
+    elif len(sys.argv) == 3:
         host = 'localhost'
-        port = int(sys.argv[1])
+        pseudo = sys.argv[1]
+        port = int(sys.argv[2])
+    elif len(sys.argv) == 2:
+        host = 'localhost'
+        port = 1337
+        pseudo = sys.argv[1]
     else:
         host = 'localhost'
         port = 1337
+        pseudo = raw_input('Veuillez indiquer votre pseudo : ')
 
     # queues initialization: communication between network threads and game logic thread (which is in render)
     send_queue = Queue.Queue()
@@ -30,5 +37,5 @@ if __name__ == '__main__':
     print('network engine is running')
 
     # render's operations have to be in the main thread.
-    display = render.Render(send_queue, receive_queue, '../shared/village.map')
+    display = render.Render(send_queue, receive_queue, '../shared/village.map', pseudo)
     display.run()
